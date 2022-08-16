f4d765003cf7575f56f63cf3f3bae9bdf2756080
ruzaigh <ruzaigh.kalam2@gmail.com>
2022-08-16 18:00:28 +0200

    added componets @Todo auth

--- src/app/features/auth/auth.module.ts
+++ src/app/features/auth/auth.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthComponent } from './auth.component';
 import {IonicModule} from "@ionic/angular";
+import {ReactiveFormsModule} from "@angular/forms";
 @NgModule({
   declarations: [
     AuthComponent
   ],
   imports: [
     CommonModule,
-    IonicModule
+    IonicModule,
+    ReactiveFormsModule
   ]
 })
 export class AuthModule { }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'todoapp-75f25',
+    appId: '1:175059779135:web:f5e2ec81154cc23809028a',
+    storageBucket: 'todoapp-75f25.appspot.com',
+    apiKey: 'AIzaSyBBlXYIG4TB1I3KlJPRAjzmtSyfXNxdjTY',
+    authDomain: 'todoapp-75f25.firebaseapp.com',
+    messagingSenderId: '175059779135',
+    measurementId: 'G-PBHTHRL6WZ',
+  },
   production: true
 };
