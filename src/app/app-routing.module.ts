f3e45943de94ff51cabbfbf31e9198e1a76738ae
ruzaigh <ruzaigh.kalam2@gmail.com>
2022-08-16 16:44:44 +0200

    added componets @Todo auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AuthComponent} from "./features/auth/auth.component";
+import {HomeComponent} from "./features/home/home.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./features/auth/auth.module').then(m => m.AuthModule),
+    component: AuthComponent
+  },
+  {
+    path: 'home/:id',
+    loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule),
+    component: HomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
